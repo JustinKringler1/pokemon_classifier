399fe3d32e3568851795ce77b0b608064a2b73c4
Justin Kringler <Justin.Kringler1@gmail.com>
2022-08-14 19:40:30 -0500

    add libraries

---
+++ pt00_config_data_load.R
@@ -0,0 +1,15 @@
+# Title: pt00_config_data_load
+# Author: Justin Kringler
+# Date: 9/14/2022
+
+# Purpose: Establish Libraries and Data for the pokemon classifier
+
+# Warnings: NA
+
+##### Libraries -----
+library(tidyverse) #for dplyrs
+library(tidymodels) #for recipes
+library(ranger) #for random forest
+library(vip) #for model impact evaluation
+
+##### Data Loading -----
